0e8873955182c92fa778d297a9ec6574a5eeb2ae
User-PC <PROFESSIONAL>
2024-07-19 10:48:45 +0500

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Post'], function() {
+    Route::get('/posts', 'IndexController');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
